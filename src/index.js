ede44dc3865e528347a0d7973c63b4be5a5aa1be
marcosBusto <117916043+marcosBusto@users.noreply.github.com>
2022-11-18 12:10:46 +0100

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const engine = require('ejs-mate');
+const path = require('path');
+
+//initializations
+const app = express();
+
+//settings
+app.engine('ejs', engine);
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+//app.set('views', '')
+
+// routes
+app.use(require('./routes/index'));
+
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+//starting server
+app.listen(3000, () => {
+    console.log('server on port 3000');
+});
---
+++ src/public/js/main.js
@@ -0,0 +1,15 @@
+var map = L.map('map-template').setView([51.505, -0.09], 3);
+
+const tileURL = 'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png' 
+const tileURL2 = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
+
+const tile = L.tileLayer(tileURL2);
+
+map.locate({enableHighAccuracy: true})
+map.on('locationfound', (e) => {
+  const coords = [e.latlng.lat, e.latlng.lng];
+  const newMarker = L.marker(coords);
+  newMarker.bindPopup('You are Here!');
+  map.addLayer(newMarker);
+  socket.emit('userCoordinates', e.latlng);
+});

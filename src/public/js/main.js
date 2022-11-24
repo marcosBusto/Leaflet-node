ced523e23e2c6032f6a96bb7bb87d005be903d2b
marcosBusto <117916043+marcosBusto@users.noreply.github.com>
2022-11-24 22:41:41 +0100

    si

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,15 +1,26 @@
-var map = L.map('map-template').setView([51.505, -0.09], 3);
-
-const tileURL = 'https://b.tile.openstreetmap.org/{z}/{x}/{y}.png' 
-const tileURL2 = 'https://maps.wikimedia.org/osm-intl/{z}/{x}/{y}.png';
-
-const tile = L.tileLayer(tileURL2);
-
-map.locate({enableHighAccuracy: true})
-map.on('locationfound', (e) => {
-  const coords = [e.latlng.lat, e.latlng.lng];
-  const newMarker = L.marker(coords);
-  newMarker.bindPopup('You are Here!');
-  map.addLayer(newMarker);
-  socket.emit('userCoordinates', e.latlng);
+//variables que contienen los puntos (centro de despliegues y marcadores)
+var center = [51.505, -0.09];
+//capas base
+var osm = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png');
+var dark = L.tileLayer('https://basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png');
+//mapa
+var map = L.map('map', {
+        layers: [dark,osm],
+        center: center,
+        zoom: 14
 });
+//marcadores
+var singleMarker = L.marker(center);
+singleMarker.addTo(map);
+var popup = singleMarker.bindPopup('This is a popup');
+popup.addTo(map);
+//control de capas
+var baseLayers = {
+"Dark": dark,
+"OpenStreetMap": osm
+};
+var overlays = {
+"Marker": singleMarker
+};
+var control = L.control.layers(baseLayers,overlays);
+control.addTo(map);
